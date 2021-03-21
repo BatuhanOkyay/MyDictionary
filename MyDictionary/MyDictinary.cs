79f8239d44fa64aa215ec26541dceb9e4c77d131
TOSHIBA <TOSHIBA@DESKTOP-HV86US1>
2021-03-21 19:10:57 +0300

    Add project files.

---
+++ MyDictionary/MyDictinary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictinary<MyKey,MyValue> // bu class mykey ve myvalue ile çalışan bir class.
+    {
+        (MyKey, MyValue)[] array;
+
+        // * constructor ilk açtığımızda bir kez çalışır ve kapanır.
+        // * ilk açtığımızda sıfır elemanı olması gerekli.
+        public MyDictinary()
+        {
+            array = new (MyKey, MyValue)[0]; // * burada başlangıcı 0 olarak belirledik.
+        }
+
+        // * ekleme operasyonunu kendimiz yazıyoruz.
+        // * mykey müşteri numaramızı tutuyor myvalue ise müşteri ismimizi.
+        // * her müşteri eklendiğinde değerimizin 1 artması gerekiyor bunun için bir kod yazıyoruz.
+        public void Add(MyKey customerNo,MyValue customerName)
+        {
+            // * geçici bir array oluşturduk ve arraya eşitledik.Çünkü her newlediğimiz de eski verimizi kaybedicez.
+            (MyKey, MyValue)[] tempArray = array;
+            // * yeni arrayimiz bi önceki arrayim hepsini alıcak ve onun üstüne +1 yeni eklediğimizi alıcak hepsi tutulacak.
+            array = new (MyKey, MyValue)[array.Length + 1];
+            // * bu tuttuğumuz değerleri dönmemiz gerekli.
+            for (int i = 0; i < tempArray.Length; i++) // geçiçi arrayin uzunluğu kadar dönücek.
+            {
+                array[i] = tempArray[i]; 
+            }
+            array[array.Length - 1] = (customerNo, customerName);
+        }
+        // bir tane de kaç adet değer var sayma operasyonu yazıyorum.
+        public int Count
+        {
+            get { return array.Length; }
+        }
+
+        // musterileri listelemek istersek şu şekilde yapabiliriz.
+        public void Listeleme()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Müşteri Adı ve Müşteri Numarası: " + array[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // normal dictionary kullanmı bu şekilde
+
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*Normal Dictionary Başlangıç-*-*-*-*-*-*-*");
+
+            Dictionary<string, int> musteriler = new Dictionary<string, int>();
+            musteriler.Add("Azra" ,1234); // ekleme operasyonu kendinden vardır.
+            Console.WriteLine("Müşteri Sayısı : " +musteriler.Count); // kaç elamanı var saymak istersek yine kendinden var
+
+            Console.WriteLine("-*-*-*-*-*-*-*-*--* Normal Dictionary Sonu-*-*-*-*-*-*-*-*-*-* ");
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("-*-*-*-*-*-*-*-*--* Kendi Dictionary Başlangıcımız-*-*-*-*-*-*-*-*-*-* ");
+
+
+            // biz kendi dictionary mizi yazacağız.
+            MyDictinary<string,int> musteriler2 = new MyDictinary<string,int>();
+            musteriler2.Add("Batuhan" , 4567);
+            musteriler2.Add("İbrahim", 1234);
+            musteriler2.Add("Halil", 9876);
+            Console.WriteLine("Müşteri sayısı : " + musteriler2.Count);
+            musteriler2.Listeleme();
+
+            Console.WriteLine("-*-*-*-*-*-*-*-*--* Kendi Dictionary Sonu-*-*-*-*-*-*-*-*-*-* ");
+
+        }
+    }
+}
